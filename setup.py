4a62bed2a1e352c9f27a889d01a2a6cf6d610154
Oleksandr Polieno (nanvel) <polyenoom@gmail.com>
2016-05-26 11:15:56 +0300

    updated setup.py, django version < 1.7

--- setup.py
+++ setup.py
@@ -54,7 +54,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'django',
-        'Pillow',
+        'django<1.7',
+        'South',
+        'Pillow'
     ],
 )
